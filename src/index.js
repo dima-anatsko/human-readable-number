4ba668370d9411d2b20f520b0d5f3a1801910c3c
Dmitry Anatsko <anatskodr@gmail.com>
2021-03-10 17:28:06 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const units = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    let result = "";
+    if (number === 0) {
+        return units[0];
+    }
+    const hundred = Math.trunc(number / 100);
+    let tail = number % 100;
+    result = hundred > 0 ? units[hundred] + " hundred " : "";
+    if (tail < 20 && tail > 0) {
+        result += units[tail];
+    } else {
+        const ten = Math.trunc(tail / 10);
+        tail = tail % 10;
+        result += ten > 0 ? tens[ten] + " " : "";
+        result += tail > 0 ? units[tail] : "";
+    }
+    return result.trim();
+};
